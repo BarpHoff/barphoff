162f56518a61c53dd96df426af37d711b726b065
ViniciusGavioli <viniciusgavioli528@gmail.com>
2026-03-26 13:23:11 -0300

    Fix LPs serving in Next.js public directory

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/home-care',
+        destination: '/home-care/index.html',
+      },
+      {
+        source: '/medicamento-alto-custo',
+        destination: '/medicamento-alto-custo/index.html',
+      },
+      {
+        source: '/negativa-cirurgia',
+        destination: '/negativa-cirurgia/index.html',
+      },
+      {
+        source: '/negativa-plano-de-saude',
+        destination: '/negativa-plano-de-saude/index.html',
+      },
+      {
+        source: '/tratamento-oncologico',
+        destination: '/tratamento-oncologico/index.html',
+      },
+    ]
+  },
 }
 
 export default nextConfig
